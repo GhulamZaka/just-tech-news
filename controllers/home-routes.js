4a2f67708a123bf8a716fc1c8635cfb8e451f58e
Ghulam Mustafa Zaka <g.mustafa.zaka@gmail.com>
2022-06-21 22:42:20 -0700

    adding login and logout route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,11 +2,14 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
-router.get("/login", (req, res) => {
-  res.render("login");
-});
+// router.get("/", (req, res) => {
+//   console.log(req.session);
+
+//   // other logic...
+// });
 
 router.get("/", (req, res) => {
+  console.log(req.session);
   Post.findAll({
     attributes: [
       "id",
@@ -45,6 +48,15 @@ router.get("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
